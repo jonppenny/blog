b34e106c84df04467165344693c8a091bc28c480
Jonathan Penny <jon@jonppenny.co.uk>
2025-11-20 17:17:31 +0000

    Some resizing and refactoring

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -60,18 +60,7 @@
             </form>
         </div>
         <div class="p-5 bios-box">
-            <div class="d-flex flex-column justify-content-between gap-4">
-                <div>
-                    <div><a href="#" class="d-inline-block my-2 text-uppercase bios-link">About</a></div>
-                    <div><a href="#" class="d-inline-block my-2 text-uppercase bios-link">Contact</a></div>
-                </div>
-                <div>
-                    <div><a href="https://linkedin.com/in/jonppenny" class="d-inline-block my-2 text-uppercase bios-link">LinkedIn</a></div>
-                    <div><a href="https://github.com/jonppenny" class="d-inline-block my-2 text-uppercase bios-link">Github</a></div>
-                </div>
-            </div>
+            @include('components.side-panel')
         </div>
     </div>
-
-    <div class="w-100 p-5 bios-box"></div>
 </x-layout>
